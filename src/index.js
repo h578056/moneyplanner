9a83a4034477a99f133677b005be74d8671974fe
c625sk6 <stian.kvamme@spv.no>
2020-11-04 13:39:16 +0100

    fix index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import BudsjettApp from './budsjettplanlegger/BudsjettApp.js'
 import BudsjettApp2 from './budsjettplanlegger/BudsjettApp2.js'
 // import App from './App';
-import * as serviceWorker from './serviceWorker';
   ReactDOM.render(
     <div>
       <BudsjettApp2/>
@@ -13,7 +12,3 @@ import * as serviceWorker from './serviceWorker';
   );
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
